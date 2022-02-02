03624a89a88e0529f627981e94801efce98f4b90
Adelmo Menezes - (AMSP) <adelmo.msp@gmail.com>
2022-02-02 20:01:18 -0300

    Concluido a etapa de Implantar tela de cadastro Vakinha State, LoaderMixin, MessagesMixin e Implantar Tela

---
+++ lib/app/core/exeptions/user_notfound_exception.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/cupertino.dart';
+
+class UserNotFoundException implements Exception {}
--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:vakinha_burger_mobile/app/core/ui/vakinha_ui.dart';
+import 'package:vakinha_burger_mobile/app/core/ui/vakinha_state.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_appbar.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_button.dart';
 import 'package:vakinha_burger_mobile/app/core/ui/widgets/vakinha_textformfield.dart';
+import 'package:vakinha_burger_mobile/app/modules/auth/register/register_controller.dart';
+import 'package:validatorless/validatorless.dart';
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState
+    extends VakinhaState<RegisterPage, RegisterController> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameEC = TextEditingController();
+  final _emailEC = TextEditingController();
+  final _passwordEC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +33,7 @@ class RegisterPage extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Form(
+              key: _formKey,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -37,19 +51,46 @@ class RegisterPage extends StatelessWidget {
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextformfield(label: 'Nome'),
+                  VakinhaTextformfield(
+                    label: 'Nome',
+                    controller: _nameEC,
+                    validator: Validatorless.required('Nome Obrigatório'),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextformfield(label: 'E-mail'),
+                  VakinhaTextformfield(
+                    label: 'E-mail',
+                    controller: _emailEC,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('E-mail obrigatório'),
+                      Validatorless.email('E-mail inválido'),
+                    ]),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextformfield(label: 'Senha'),
+                  VakinhaTextformfield(
+                    label: 'Senha',
+                    controller: _passwordEC,
+                    obscureText: true,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Senha obrigatório'),
+                      Validatorless.min(
+                          6, 'Senha deve conter pelo menos 6 caracteres'),
+                    ]),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
-                  const VakinhaTextformfield(label: 'Confirmar senha'),
+                  VakinhaTextformfield(
+                    label: 'Confirmar senha',
+                    obscureText: true,
+                    validator: Validatorless.multiple([
+                      Validatorless.required('Confirma senha obrigatória'),
+                      Validatorless.compare(_passwordEC, 'Senha diferente'),
+                    ]),
+                  ),
                   const SizedBox(
                     height: 30,
                   ),
@@ -57,7 +98,16 @@ class RegisterPage extends StatelessWidget {
                     child: VakinhaButton(
                       width: context.width,
                       label: 'CADASTRAR',
-                      onPressed: () {},
+                      onPressed: () {
+                        final formValid =
+                            _formKey.currentState?.validate() ?? false;
+                        if (formValid) {
+                          controller.register(
+                              name: _nameEC.text,
+                              email: _emailEC.text,
+                              password: _passwordEC.text);
+                        }
+                      },
                     ),
                   ),
                 ],
